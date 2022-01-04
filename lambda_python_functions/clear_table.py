010f81f67177e99feb2083be257e59cbbc0a1251
Logan Meyer <loganmeyer@Logans-Mac-Pro.local>
2022-01-03 21:53:01 -0700

    actions

---
+++ lambda_python_functions/clear_table.py
@@ -0,0 +1,31 @@
+import boto3
+dynamo = boto3.resource('dynamodb')
+
+def truncateTable(tableName):
+    table = dynamo.Table(tableName)
+
+    #get the table keys
+    tableKeyNames = [key.get("AttributeName") for key in table.key_schema]
+
+    #Only retrieve the keys for each item in the table (minimize data transfer)
+    projectionExpression = ", ".join('#' + key for key in tableKeyNames)
+    expressionAttrNames = {'#'+key: key for key in tableKeyNames}
+
+    counter = 0
+    page = table.scan(ProjectionExpression=projectionExpression, ExpressionAttributeNames=expressionAttrNames)
+    with table.batch_writer() as batch:
+        while page["Count"] > 0:
+            counter += page["Count"]
+            # Delete items in batches
+            for itemKeys in page["Items"]:
+                batch.delete_item(Key=itemKeys)
+            # Fetch the next page
+            if 'LastEvaluatedKey' in page:
+                page = table.scan(
+                    ProjectionExpression=projectionExpression, ExpressionAttributeNames=expressionAttrNames,
+                    ExclusiveStartKey=page['LastEvaluatedKey'])
+            else:
+                break
+    print(f"Deleted {counter}")
+
+truncateTable("YOUR_TABLE_NAME")
