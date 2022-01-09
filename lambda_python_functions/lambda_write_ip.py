76054bf2836234c0f1d3e055c82474a8b9e29fe1
Logan Meyer <loganmeyer@Logans-Mac-Pro.local>
2022-01-08 21:46:55 -0700

    python lambda revisions

--- lambda_python_functions/lambda_write_ip.py
+++ lambda_python_functions/lambda_write_ip.py
@@ -10,50 +10,25 @@ def write_ip_datetime_handler(ip, datetime, dynamodb=None):
         dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
 
     table = dynamodb.Table('visitors')
-    response = table.put_item(
+    putresponse = table.put_item(
        Item={
             'ip': ip,
             'datetime': datetime,
             }
-        }
     )
+
+    return putresponse
+
+    response = table.scan()
+    data = response['Items']
+
+    #Scan has 1 MB limit on the amount of data it will return in a request, so we need to paginate through the results in a loop.
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+
+    for item in data:
+        print(item['ip'], ":", item['datetime'])
+
     return response
-
-def query_visitors(ip, datetime, dynamodb=None):
-    if not dynamodb:
-        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
-
-    table = dynamodb.Table('visitors')
-    print(f"Get count of unique ip, get separate count of unique ip & datetime")
-    ##print(f"Get year, title, genres, and lead actor")
-
-    # Expression attribute names can only reference items in the projection expression.
-    response = table.query(
-        ProjectionExpression="#yr, title, info.genres, info.actors[0]",
-        ExpressionAttributeNames={"ip": "ip"},
-        KeyConditionExpression=
-            Key('year').eq(year) & Key('title').between(title_range[0], title_range[1])
-    )
-    return response['Items']
-
-
-
-
-if __name__ == '__main__':
-    visitor_resp = write_ip_datetime_handler("192.168.0.0", "12/12/0000 "
-        + randint(0, 24)":"
-        + randint(0, 60)":"
-        + randint(0, 60)
-        , 0)
-    print("Put ip & datetime succeeded:")
-    pprint(visitor_resp, sort_dicts=False)
-
-if __name__ == '__main__':
-    query_year = 1992
-    query_range = ('A', 'L')
-    print(f"Get movies from {query_year} with titles from "
-          f"{query_range[0]} to {query_range[1]}")
-    movies = query_visitors(query_year, query_range)
-    for movie in movies:
-        print(f"\n{movie['year']} : {movie['title']}")
-        pprint(movie['info'])
+    return data
