dfdb422f57d2a6d695b139fbeb695bb41c995ee9
Logan Meyer <loganmeyer@Logans-Mac-Pro.local>
2022-01-10 09:49:45 -0700

    Update function.py

--- lambda_python_functions/function.py
+++ lambda_python_functions/function.py
@@ -4,7 +4,7 @@ print('Loading function')
 
 def lambda_handler(event, context):
 	#1. Parse out query string params
-	transactionId = event['queryStringParameters']['transactionId']
+	transactionId = event['queryStringParameters']['transactionid']
 	transactionType = event['queryStringParameters']['type']
 	transactionAmount = event['queryStringParameters']['amount']
 
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
 
 	#2. Construct the body of the response object
 	transactionResponse = {}
-	transactionResponse['transactionId'] = transactionId
+	transactionResponse['transactionid'] = transactionId
 	transactionResponse['type'] = transactionType
 	transactionResponse['amount'] = transactionAmount
 	transactionResponse['message'] = 'Hello from Lambda land'
@@ -22,9 +22,9 @@ def lambda_handler(event, context):
 	#3. Construct http response object
 	responseObject = {}
 	responseObject['statusCode'] = 200
-    responseObject['headers'] = {}
-    #responseObject['headers']['Access-Control-Allow-Origin'] = '*'
-    responseObject['headers']['Content-Type'] = 'application/json'
+	responseObject['headers'] = {}
+	responseObject['headers']['Content-Type'] = 'application/json'
+	responseObject['headers']['Access-Control-Allow-Origin'] = '*'
 	responseObject['body'] = json.dumps(transactionResponse)
 
 	#4. Return the response object
