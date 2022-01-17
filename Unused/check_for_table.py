39d7522977e1f665b5e09fe419e1621f2ecb9c54
Logan T. Meyer <logan.t.meyer@gmail.com>
2022-01-16 21:41:35 -0700

    cleanup

--- Unused/check_for_table.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-dynamodb_client = boto3.client('dynamodb')
-
-try:
-    response = dynamodb_client.describe_table(TableName='test')
-except dynamodb_client.exceptions.ResourceNotFoundException:
-    # do something here as you require
-    pass
